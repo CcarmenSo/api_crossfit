9aaf6bf99bc3e8465afbb093fbf3d1d8965498c6
Carmen <carsomu87@gmail.com>
2024-11-06 21:04:42 +0100

    creación de modelos

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -6,5 +6,47 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    //
+    public $timestamps = false;
+    protected $table = 'categoria';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'nombre',
+    ];
+
+    //En funcion de si la categoria llega informada EL id o no, realizamos la consulta de una categoria o de todas las categroias
+    public static function consulta ($id = null){
+        if($id){
+            return Categoria::where('id', $id)->get();
+        }else{
+            return Categoria::orderBy('nombre')->get();
+        }
+    }
+
+    //Hasmany nos permite acceder a todas las peliculas de una categoria
+    public function ejercicios(){
+        return $this->hasMany(Ejercicio::class, 'idcategoria', 'id');
+    }
+
+    
+    //consultarremos solo la categoria que nos llega por parametro o todas si no nos llega ninguna
+  
+    public static function consultaEjercicios($id = null) {
+        if ($id) {
+            $categorias = Categoria::where('id', $id)->get();
+        } else {
+            $categorias = Categoria::orderBy('nombre')->get();
+        }
+    
+        foreach ($categorias as $categoria) {
+            $categoria->ejercicios->each(function($ejercicio) {
+            $ejercicio->imagen = env('URL_IMAGES').$ejercicio->imagen;
+            });
+        }
+    
+        return $categorias;
+    }
+    
+    
+ 
 }
+
--- app/Models/Ejercicio.php
+++ app/Models/Ejercicio.php
@@ -23,6 +23,53 @@ class Ejercicio extends Model
         
     ];
 
-    //método estatico para consultar los datos de la categoria de cada pelicula utilizando el método Elocuent belongsTo()
+    //método NO estatico para consultar los datos de la categoria de cada ejercicios utilizando el método Elocuent belongsTo()
+    //Esto indica que los ejercicios pertenecen a categoria, idcategoria es la clave foranea e id es la clave primaria
+    public function categoria (){
+        return $this->belongsTo(Categoria::class, 'idcategoria', 'id');
+    }
+
+    //Creamos el modelo de consulta de todos los ejercicios por filtro de nombre de pelicula y de id categoria
+
+    public static function consulta($filtro, $categoria){
+        if($categoria){
+            $ejercicios = Ejercicio::where('nombre', 'like', "%$filtro%")->where('idcategoria', $categoria)
+            ->orderBy('nombre')
+            ->get();
+        } else{
+            $ejercicios = Ejercicio::where('nombre', 'like', "%$filtro%")
+            ->orderBy('nombre')
+            ->get();
+        }
+
+        foreach ($ejercicios as $ejercicio){
+            $ejercicio->categoria;
+            $ejercicio->imagen = env('URL_IMAGES').$ejercicio->imagen;
+        }
+        return $ejercicios;
+    }
+    
+    //metodo para consultar el ejercicio por su id
+    public static function consultaId($id){
+        $ejercicio = Ejercicio::find($id);
+        if($ejercicio){
+        $ejercicio->categoria;
+        $ejercicio->imagen = env('URL_IMAGES').$ejercicio->imagen;
+        }
+        return $ejercicio;
+
+    }
+
+    //metodo para dar de alta una pelicula a través del array de datos que llegue en el parametro de entrada
+    public function alta($datos){
+        return Ejercicio::create([
+        'nombre'=>$datos['nombre'],
+        'idcategoria'=>$datos['idcategoria'],
+        'direccion'=>$datos['direccion'],
+        'nivel'=>$datos['nivel'],
+        'imagen'=>$datos['imagen'],
+        'video'=>$datos['video'],
+        ]);
+    }
 
 }
